c768234bb0aec08dd2dcb800ceb392f3a09cc4b2
druy3april <druy3april@gmail.com>
2026-09-12 11:37:36 +0700

    Update dim_channels.sql

--- dbt_project/models/dim_channels.sql
+++ dbt_project/models/dim_channels.sql
@@ -29,4 +29,21 @@ SELECT
         ELSE sales_name 
     END as tiktok_stats_name
 
+    CASE 
+        -- Các kênh đã được map chuẩn hóa:
+        WHEN sales_name ILIKE '%Cảnh%' THEN 'https://xaxqebblrstsaazrqowb.supabase.co/storage/v1/object/public/avatars/20260909-153623.webp'
+        WHEN sales_name ILIKE '%Huy%'   THEN 'https://xaxqebblrstsaazrqowb.supabase.co/storage/v1/object/public/avatars/20260909-153604.webp'
+        WHEN sales_name ILIKE '%Jocelyn%' THEN 'https://xaxqebblrstsaazrqowb.supabase.co/storage/v1/object/public/avatars/20260909-153523.webp'
+        WHEN sales_name ILIKE 'Tiktok %t' AND sales_name NOT LIKE '%Huy%' THEN 'URL_ẢNH_CỦA_ĐẠT_GENFARMER'
+        WHEN sales_name ILIKE '%Thiên%' THEN 'https://xaxqebblrstsaazrqowb.supabase.co/storage/v1/object/public/avatars/20260909-153556.webp'
+        
+        -- Các kênh Insta và kênh lẻ khác đang có trong Supabase:
+        WHEN sales_name ILIKE '%N.D.K.L%' THEN 'https://xaxqebblrstsaazrqowb.supabase.co/storage/v1/object/public/avatars/20260909-153523.webp'
+        WHEN sales_name ILIKE '%Quang Vũ%' THEN 'https://xaxqebblrstsaazrqowb.supabase.co/storage/v1/object/public/avatars/20260909-153709.webp'
+        WHEN sales_name ILIKE '%Lynette%' THEN 'https://xaxqebblrstsaazrqowb.supabase.co/storage/v1/object/public/avatars/20260909-153652.webp'
+        WHEN sales_name ILIKE '%genboxphone%' THEN 'https://xaxqebblrstsaazrqowb.supabase.co/storage/v1/object/public/avatars/20260909-153652.webp'
+        WHEN sales_name ILIKE '%Việt%' THEN 'https://xaxqebblrstsaazrqowb.supabase.co/storage/v1/object/public/avatars/20260909-153612.webp'
+        ELSE NULL 
+    END as avatar_url
+
 FROM raw_sales
